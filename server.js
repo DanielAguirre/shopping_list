99925d49e6ce3c7ab70df24fe30dc22edf8e77f7
DanielAguirre <danter1@gmail.com>
2015-03-09 22:30:33 -0600

    Se establecio la configuracion basica para arrancar un servidor en el archio server.js

--- server.js
+++ server.js
@@ -1,2 +1,15 @@
 var http = require('http'),
-    conf = require('./conf');
+    conf = require('./conf'),
+    expressServer = require('./app/expressServer');
+
+var app = new expressServer();
+
+var server = new http.createServer(app.expressServer);;
+
+if(module.parent){
+	server.listen(conf.port,function{
+		console.log('app runining in port 3000')
+	});
+}else {
+	module.exports = server;
+}
