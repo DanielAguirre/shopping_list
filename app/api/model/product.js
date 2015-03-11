eda8065dd5a47ef6e7676e555031cf987a998dbd
DanielAguirre <danter1@gmail.com>
2015-03-10 21:07:44 -0600

    Creado schema de listas

--- app/api/model/product.js
+++ app/api/model/product.js
@@ -1,6 +1,6 @@
 var Sequelize = require('sequelize');
 
-var Product = sequelize.define('Product',{
+var Product = sequelize.define('products',{
 	'product': {Sequelize.TEXT, allowNull: false},
 	'price': {Sequelize.FLOAT(11,12), , allowNull: false}
 })
---
+++ app/api/model/shoppinlist.js
@@ -0,0 +1,11 @@
+var Sequelize = require('sequelize'),
+	product = require('./product');
+	
+var List = sequelize.define('list',{
+	'total'	:{Sequelize.FLOAT(11,12), , allowNull: false}
+	'timestamps':true
+});
+
+List.hasMany(product);
+
+module.export = List;
