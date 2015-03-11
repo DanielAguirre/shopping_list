13ff6cb998d8b593c5e14f4d40e2fcc85db7ee79
DanielAguirre <danter1@gmail.com>
2015-03-10 20:38:09 -0600

    Se agrego el middleware bodyParser

--- app/expressServer.js
+++ app/expressServer.js
@@ -1,11 +1,14 @@
 var env = process.env.NODE_EN || 'production',
     express = require('express'),
-    swig = require('swig');
+    swig = require('swig'),
+    bodyParser = require('body-parser');
 
 var ExpressServer = function (){
 
     this.expressServer = express()
 
+    //middlewares
+    this.expressServer.use(bodyParser.json());
     if(env === 'development') {
     	console.log("NO HAY CHACHE");
     	this.expressServer,set('view cache', false);
